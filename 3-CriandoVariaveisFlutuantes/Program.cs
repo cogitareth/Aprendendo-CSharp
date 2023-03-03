ca5045af6bba6ef9c4cb0810281c7720d4d8d1a3
Gabriel Henrique <turles41@gmail.com>
2023-03-03 02:50:49 -0300

    Adicionar arquivos de projeto.

---
+++ 3-CriandoVariaveisFlutuantes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variáveis Ponto flutuante");
+
+        double salario;
+        salario = 3176.13;
+
+        salario = 3000;
+
+        double idade;
+        idade = 7.0 / 5;
+
+        Console.WriteLine(idade);
+        Console.WriteLine("Meu salário é de R$" + salario + "!");
+        
+        Console.WriteLine("Tecle enter para fechar!");
+        Console.ReadLine();
+    }
+}
