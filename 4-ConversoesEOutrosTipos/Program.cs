53aa2b47c3c0baf9dcb2276f1b6f2e6edc60fa6a
Gabriel Henrique <turles41@gmail.com>
2023-03-03 03:15:23 -0300

    Adicionar o projeto 4.

---
+++ 4-ConversoesEOutrosTipos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 4 - Conversões e outros tipos");
+
+        double salario = 3000.15;
+
+        int salarioInteiro = (int)salario;
+        Console.WriteLine(salarioInteiro);  
+
+        //tipo de variável de 64 bits
+        long x = 200000000000;
+        Console.WriteLine(x);
+
+        //tipo de variável com 16 bits
+        short y = 1500;
+        Console.WriteLine(y);
+
+        float altura = 1.62f;
+        Console.WriteLine(altura);
+        
+        
+        Console.WriteLine("Tecle enter para fechar!");
+        Console.ReadLine();
+    }
+}
